8de869123c6fb997d2abf9fda305b85adbe39281
Jacob Koziej <jacobkoziej@gmail.com>
2024-09-28 22:42:22 -0400

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "The Cooper Union - ECE 416: Adaptive Algorithms";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        python = pkgs.python3;
+
+        python-pkgs = python.withPackages (
+          python-pkgs: with python-pkgs; [
+            numpy
+            scipy
+          ]
+        );
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ python-pkgs ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
