8d84ab1f14e5b783124edb4dae10edc2ab1ada74
Jacob Koziej <jacobkoziej@gmail.com>
2024-09-30 12:08:05 -0400

    Add `notebook.py`

---
+++ ps/1/notebook.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# noteboook.py -- ps1: initial experimentation
+# Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
