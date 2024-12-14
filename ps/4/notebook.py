d487df0749f257d794f10733fe01eada9d7343e7
Jacob Koziej <jacobkoziej@gmail.com>
2024-12-13 22:15:10 -0500

    Add `Preliminary Analysis`

--- ps/4/notebook.py
+++ ps/4/notebook.py
@@ -17,3 +17,71 @@
 #
 # noteboook.py -- ps4: kalman filters
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+# ruff: noqa: E402
+
+# %%
+import numpy as np
+
+from numpy.linalg import (
+    eigvals,
+    matrix_rank,
+)
+
+# %% [markdown]
+# ## Preliminary Analysis
+
+# %%
+A_2 = np.array(
+    [
+        [-3, -5],
+        [1, 1],
+    ]
+)
+A_1 = A_2 / 2
+
+C = np.array([[1, 0]])
+
+Q_x = np.array(
+    [
+        [0.1, 0],
+        [0, 0.1],
+    ]
+)
+Q_y = np.array(0.05)
+
+# %%
+A_1_eig = eigvals(A_1)
+
+# %% tags=["active-ipynb"]
+A_1_eig
+
+# %%
+A_2_eig = eigvals(A_2)
+
+# %% tags=["active-ipynb"]
+A_2_eig
+
+# %% [markdown]
+# $A_1$ is the stable system since the magnitude of its eigen values are
+# all less than 1.
+
+# %%
+N = A_1.shape[-1]
+
+# %%
+O_1 = np.concatenate(
+    [
+        C @ np.eye(N),
+        C @ A_1,
+    ]
+)
+O_2 = np.concatenate(
+    [
+        C @ np.eye(N),
+        C @ A_2,
+    ]
+)
+
+assert matrix_rank(O_1) == N
+assert matrix_rank(O_2) == N
